242bb96aa2a04158345aa85114c9e876f918c5a1
kocaer7 <kocaer7@outlook.com>
2018-06-06 14:24:55 +0200

    New Pages/Navbar

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class PagesController extends Controller
+{
+    public function index(){
+        $title = 'Welcome To Laravel!';
+        //return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
+    }
+    public function about(){
+        $title = 'About Us';
+        return view('pages.about')->with('title', $title);
+    }
+    public function services(){
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Voetbal', 'Basketball', 'E-sports']
+        );
+        return view('pages.services')->with($data);
+    }
+}
---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //$posts = Post::all();
+        //return Post::where('title', 'Post Two')->get();
+        //$posts = DB::select('SELECT * FROM posts');
+        //$posts = Post::orderBy('title','desc')->take(1)->get();
+        //$posts = Post::orderBy('title','desc')->get();
+        $posts = Post::orderBy('created_at','desc')->paginate(10);
+        return view('posts.index')->with('posts', $posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Post::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
