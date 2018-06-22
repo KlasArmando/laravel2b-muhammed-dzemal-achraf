9dee45b6a21759af8e72c62eef5e81cb5616ab72
traktor12 <dze-mo@hotmail.com>
2018-06-22 18:21:28 +0200

    Update Crud3Factory.php

--- database/factories/Crud3Factory.php
+++ database/factories/Crud3Factory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Crud::class, function (Faker $faker) {
+$factory->define(App\Crud3::class, function (Faker $faker) {
     return [
         //
     ];
